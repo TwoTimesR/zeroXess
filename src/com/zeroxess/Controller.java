06d7ff1da5af2cfa2908429bb0ee48e4b6c1fd8a
Brent19059825 <DaddyLongNeck7a!>
2020-05-07 21:16:46 +0200

    Login screen added

--- src/com/zeroxess/Controller.java
+++ src/com/zeroxess/Controller.java
@@ -1,9 +1,37 @@
 package com.zeroxess;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Text messageField;
 
     public void initialize() {
 
     }
 
+    public void loginButtonAction(ActionEvent actionEvent) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if(username.equals("admin") && password.equals("Test")){
+            messageField.setText("Login successful");
+            //TODO: Send to next scene
+
+        }else{
+            messageField.setText("Login incorrect");
+        }
+
+    }
 }
