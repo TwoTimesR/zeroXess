b8c084abfd95ed0d5fb25730e3c62e176c2ab4af
Tonio Stocks <19091303@student.hhs.nl>
2020-05-11 20:50:19 +0200

    added class liveStock to get

--- src/com/zeroxess/LiveStock.java
+++ src/com/zeroxess/LiveStock.java
@@ -3,14 +3,20 @@ package com.zeroxess;
 import java.util.ArrayList;
 
 public class LiveStock {
+    private String name;
     private Integer amount;
     ArrayList<LiveStock> liveStocks = new ArrayList<>();
+
+    public LiveStock(String name, Integer amount){
+        this.name = name;
+        this.amount = amount;
+    }
 
     public Integer getAmount() {
         return amount;
     }
 
-    public void setAmount(Integer amount) {
-        this.amount = amount;
+    public String getName() {
+        return name;
     }
 }
