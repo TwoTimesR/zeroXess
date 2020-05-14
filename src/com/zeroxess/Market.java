58d0df9e162e31c14cf6e43235494afeba7a4b1f
2R <63011073+RichalRambaran@users.noreply.github.com>
2020-05-14 18:43:41 +0200

    minor changes 4

--- src/com/zeroxess/Market.java
+++ src/com/zeroxess/Market.java
@@ -6,7 +6,7 @@ public class Market {
     private ArrayList<Order> orders = new ArrayList<>();
 
     public Market() {
-        //
+        //purely exists out of orders
     }
 
     public ArrayList<Order> getOrders() {
@@ -33,34 +33,28 @@ public class Market {
     }
 
     private Boolean hasEnoughSellingItem(/*unsure who to put here*/) {
-        /*
-        checks weather the seller has the right amount to sell
-        e.g. cant sell 10 items if you only have 3
-        */
+        //checks weather the seller has the right amount to sell
         return true;
     }
 
     private void removeSoldItemsFromSeller(/*unsure who to put here*/) {
-        //
+        //removes selling item from seller
     }
 
     private void addProfitToSeller(/*unsure who to put here*/) {
-        //
+        //adds money to sellers
     }
 
     private Boolean hasEnoughMoney(/*unsure who to put here*/) {
-        /*
-        checks weather the buyer has enough money to buy
-        e.g. cant buy an order of 69.99 if you only have 30.00
-        */
+        //checks weather the buyer has enough money to buy
         return true;
     }
 
     private void removeSpentMoneyFromBuyer(/*unsure who to put here*/) {
-        //
+        //removes money from buyer
     }
 
     private void addBoughtItemToBuyer(/*unsure who to put here*/) {
-        //
+        //adds selling item to buyer
     }
 }
--- src/com/zeroxess/MarketController.java
+++ src/com/zeroxess/MarketController.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -15,18 +16,10 @@ public class MarketController extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         GridPane gridPane = new GridPane();
-
         Scene startScene = new Scene(gridPane, 800, 600);
         primaryStage.setScene(startScene);
         primaryStage.setTitle("Market");
         primaryStage.show();
-
-        //test stuff
-        Label testLabel1 = new Label("This is a label");
-        gridPane.add(testLabel1, 1, 1, 1, 1);
-
-        TextField firstName = new TextField();
-        gridPane.add(firstName, 1, 2, 1, 1);
     }
 
     public static void main(String[] args) {
