dde5aed9782fa0864491ec7a0c2cebdbe0cb9f05
Tonio Stocks <19091303@student.hhs.nl>
2020-05-17 21:58:02 +0200

    heb 2 test gemaakt en LiveStockController gefixt met een verkeerde input

---
+++ Test/com/zeroxess/livestockpage/LiveStockControllerTest.java
@@ -0,0 +1,42 @@
+package com.zeroxess.livestockpage;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TextField;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LiveStockControllerTest {
+
+    TextField nameCompany;
+    TextField typeAnimal;
+    TextField nameAnimal;
+    TextField weight;
+    TextField age;
+    TextField price;
+
+
+
+    @Test
+    void getGraphic() {
+        ObservableList<Animal> animals = FXCollections.observableArrayList();
+
+        animals.add(new Animal("Project1","pig","Frank",55,4,440.69));
+        assertTrue(animals.contains("Frank"));
+    }
+
+    @Test
+    void onAddButtonCLicked() {
+        ObservableList<Animal> animals = FXCollections.observableArrayList();
+        nameCompany.setText("Testa");
+        typeAnimal.setText("cow?");
+        nameAnimal.setText("test!");
+        weight.setText("88");//input should be double
+        age.setText("5");
+        price.setText("5");
+
+        assertTrue(animals.contains("testa"));
+    }
+
+}
