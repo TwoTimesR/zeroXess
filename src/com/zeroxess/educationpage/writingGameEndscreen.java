59791b9d2c63b0ee2bfc760cf555ce98edaee074
aycan-acn <aycan.acn2020@gmail.com>
2020-06-02 00:41:37 +0200

    klaar alleen nog met homepage linken

--- src/com/zeroxess/educationpage/writingGameEndscreen.java
+++ src/com/zeroxess/educationpage/writingGameEndscreen.java
@@ -1,14 +1,21 @@
 package com.zeroxess.educationpage;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class writingGameEndscreen {
+    @FXML
+    private AnchorPane anchorPane;
 
    @FXML
     private Label total_score;
@@ -25,6 +32,15 @@ public class writingGameEndscreen {
        total_score.setText(String.valueOf(g.getScore()));
        correctAnswers.setText(String.valueOf(writingGameController.getCorrectAnswers()));
    }
+//   public void openHomePage() throws IOException{
+//       Stage stage = (Stage) anchorPane.getScene().getWindow();
+//       Parent root = FXMLLoader.load(getClass().getResource("/com/zeroxess/homepage/homepage.fxml"));
+//       Scene scene =  new Scene(root, 800 ,600);
+//       stage.setScene(scene);
+//       stage.show();
+//   }
+
+
 
 
 }
