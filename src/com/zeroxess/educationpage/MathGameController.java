99ce25b22405d4ca63c50d6e4f23c02138ea3cdb
Tonio Stocks <19091303@student.hhs.nl>
2020-06-05 15:07:59 +0200

    ben klaar met MathGmame kon de methods van game niet toepassen op the Aplicatie, michien geen genoeg fantazie. maar wil code nog aanpassen om met de score methode te werken dat displayed op endscreen.

--- src/com/zeroxess/educationpage/MathGameController.java
+++ src/com/zeroxess/educationpage/MathGameController.java
@@ -47,9 +47,16 @@ public class MathGameController {
 
     boolean gameMode;
     boolean gameDifficulty;
+    boolean correctAnswer;
+    int score;
+    int questionNumber;
 
     MathGameFactory mathGameFactory = new MathGameFactory();
-    MathGame mathGame;
+    MathGame mathDigit1;
+    MathGame mathDigit2;
+    MathGame mathDigit3;
+    MathGame mathSignOBJ1;
+    MathGame mathSignOBJ2;
 
     @FXML
     private void onStartClicked(){
@@ -107,21 +114,22 @@ public class MathGameController {
     }
 
     private void normalQuestionGen(){
-        mathGame= mathGameFactory.makeNumber();
-        number1.setText(String.valueOf(mathGame.getDigit()));
-        mathGame = mathGameFactory.makeNumber();
-        number2.setText(String.valueOf(mathGame.getDigit()));
-        mathGame = mathGameFactory.makeMathSign();
-        mathSign1.setText(mathGame.getMathSign());
+        mathDigit1 = mathGameFactory.makeNumber();
+        mathDigit2 = mathGameFactory.makeNumber();
+        mathSignOBJ1 = mathGameFactory.makeMathSign();
+
+        number1.setText(String.valueOf(mathDigit1.getDigit()));
+        number2.setText(String.valueOf(mathDigit2.getDigit()));
+        mathSign1.setText(mathSignOBJ1.getMathSign());
     }
 
     private void hardQuestionGen(){
         normalQuestionGen();
-        mathGame = mathGameFactory.makeNumber();
-        number3.setText(String.valueOf(mathGame.getDigit()));
-        mathGame = mathGameFactory.makeMathSign();
-        mathSign2.setText(mathGame.getMathSign());
+        mathDigit3 = mathGameFactory.makeNumber();
+        mathSignOBJ2 = mathGameFactory.makeMathSign();
 
+        number3.setText(String.valueOf(mathDigit3.getDigit()));
+        mathSign2.setText(mathSignOBJ2.getMathSign());
     }
 
     @FXML
@@ -129,13 +137,90 @@ public class MathGameController {
         checkAnswerButton.setVisible(false);
         nextQuestion.setVisible(true);
 
+        int answer = getAnswer();
+        checkAnswerMethod(answer);
 
     }
 
+    private int getAnswer(){
+        if (gameDifficulty ==true){
+            return anwserNormal();
+
+        }
+        else return answerHard();
+    }
+
+    private int anwserNormal(){
+        int number1 = mathDigit1.getDigit();
+        int number2 = mathDigit2.getDigit();
+        if (mathSignOBJ1.getMathSign().equals("*")){
+            return (number1 * number2);
+        }
+        if (mathSignOBJ1.getMathSign().equals("+")){
+            return (number1 + number2);
+        }
+        else return (number1 - number2);
+    }
+
+    private int answerHard(){
+        int number1 =mathDigit1.getDigit();
+        int number2 =mathDigit2.getDigit();
+        int number3 =mathDigit3.getDigit();
+        int numberAfterMultiply;
+        if(mathSignOBJ2.getMathSign().equals("*")){
+            numberAfterMultiply = (number3 * number1);
+            if (mathSignOBJ1.getMathSign().equals("*")){
+                return (numberAfterMultiply * number2);
+            }
+            if (mathSignOBJ1.getMathSign().equals("+")){
+                return (numberAfterMultiply + number2);
+            }
+            else return (numberAfterMultiply - number2);
+        }
+        if (mathSignOBJ1.getMathSign().equals("*")){
+            numberAfterMultiply = (number1 * number2);
+            if (mathSignOBJ2.getMathSign().equals("+")){
+                return (number3 + numberAfterMultiply);
+            }
+            else return (number3 - numberAfterMultiply);
+        }
+        else return 999999;
+    }
+
+    private boolean checkAnswerMethod(int answer){
+        if (answer == Integer.parseInt(AnswerTextField.getText())){
+            AnswerTextField.setText("correct");
+            AnswerTextField.setEditable(false);
+            score = score + 10;
+            scoreLabel.setText(String.valueOf(score));
+
+            return correctAnswer =true;
+        }
+        else {
+            AnswerTextField.setText("False");
+            AnswerTextField.setEditable(false);
+            score = score - 5;
+            scoreLabel.setText(String.valueOf(score));
+
+            return correctAnswer = false;
+        }
+    }
+
     @FXML
-    private void onnextQuestionClicked(){
+    private void onnextQuestionClicked() throws IOException, InterruptedException {
         checkAnswerButton.setVisible(true);
         nextQuestion.setVisible(false);
+        AnswerTextField.clear();
+        AnswerTextField.setEditable(true);
+        questionNumber ++;
+        numberQuestion.setText(String.valueOf(questionNumber));
+        if (gameMode == true) {
+            if (questionNumber != 11) {
+                createQuestion();
+            }
+            else onQuitButtonClicked();
+        }
+        else createQuestion();
 
     }
 
@@ -209,7 +294,8 @@ public class MathGameController {
         scoreName.setVisible(true);
         questionLabel.setVisible(true);
         numberQuestion.setVisible(true);
-        numberQuestion.setText("0");
+        numberQuestion.setText("1");
+        questionNumber = 1;
         numberQuestion.setVisible(true);
         backToDificultySelect.setVisible(true);
         AnswerTextField.setVisible(true);
@@ -232,15 +318,18 @@ public class MathGameController {
         mathSign1.setVisible(false);
         equalsSign.setVisible(false);
         scoreLabel.setVisible(false);
+        score = 0;
         scoreLabel.setText("0");
         scoreName.setVisible(false);
         questionLabel.setVisible(false);
         numberQuestion.setVisible(false);
         numberQuestion.setText("0");
+        questionNumber = 1;
         numberQuestion.setVisible(false);
         backToDificultySelect.setVisible(false);
         AnswerTextField.setVisible(false);
         checkAnswerButton.setVisible(false);
+        nextQuestion.setVisible(false);
         QuitButton.setVisible(false);
         mathSign2.setVisible(false);
         number3.setVisible(false);
