43ef9d7a0795205c1786e5ebb506c45264a32179
2R <63011073+RichalRambaran@users.noreply.github.com>
2020-06-10 20:09:39 +0200

    ReadingGame GUI fields done

--- src/com/zeroxess/educationpage/ReadingGameController.java
+++ src/com/zeroxess/educationpage/ReadingGameController.java
@@ -1,4 +1,50 @@
 package com.zeroxess.educationpage;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+
 public class ReadingGameController {
+    private ReadingGame readingGame = new ReadingGame("The Adventure's of Jim");
+
+    @FXML
+    Pane pane;
+
+    @FXML
+    TextArea reading_text;
+
+    @FXML
+    TextArea question_text;
+
+    @FXML
+    TextArea mc_answer_0;
+
+    @FXML
+    TextArea mc_answer_1;
+
+    @FXML
+    TextArea mc_answer_2;
+
+    @FXML
+    TextArea mc_answer_3;
+
+    @FXML
+    Button previous_question;
+
+    @FXML
+    Button confirm_answer;
+
+    @FXML
+    Button next_question;
+
+    @FXML
+    Label score;
+
+    @FXML
+    Label total_score_integer;
+
+    @FXML
+    Button back;
 }
