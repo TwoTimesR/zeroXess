adbd1be05dbc10819460ef29e9fbd02d1691b693
Bimbamboe <vincent_groeneveld@hotmail.nl>
2020-06-10 19:00:33 +0200

    Profielpagina laad nu gegevens vanuit UserProfile en niet langer uit txt bestandje

--- Test/com/zeroxess/profilepage/ProfilePageControllerTest.java
+++ Test/com/zeroxess/profilepage/ProfilePageControllerTest.java
@@ -10,13 +10,15 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfilePageControllerTest {
-    private  ProfilePageController profile = new ProfilePageController();
+    private ProfilePageController profile = new ProfilePageController();
+}
+    //Deze test was voor Project 1. Voor project 2 komt deze te vervallen omdat er nu gebruik wordt gemaakt van een UserProfile.
 
     /*
     In deze test schrijf ik in het .txt bestand een voornaam en een achternaam. Vervolgens gebruik
     ik een methode die alle gegevens laadt en dat vergelijk ik om te kijken of het getInformatie Methode,
     gegevens succesvol kan laden.
-    */
+
     @Test
     public void showTxtFile() throws IOException {
         List<String> TestTxtFile = Files.readAllLines(Paths.get("ProfileInfo.txt"));
@@ -24,4 +26,4 @@ class ProfilePageControllerTest {
         TestTxtFile.set(1,"Brown");
         assertEquals(profile.getInformatieTxt(),TestTxtFile);
     }
-}
+*/
