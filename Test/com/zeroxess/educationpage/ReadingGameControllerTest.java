4bf0633442e0d7ec5390f83941ae1f1bfa96b8c3
2R <63011073+RichalRambaran@users.noreply.github.com>
2020-06-15 20:39:15 +0200

    WritingGameControllerTest class made

---
+++ Test/com/zeroxess/educationpage/ReadingGameControllerTest.java
@@ -0,0 +1,11 @@
+package com.zeroxess.educationpage;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReadingGameControllerTest {
+    @Test
+    public void someStuff() {
+
+    }
+}
