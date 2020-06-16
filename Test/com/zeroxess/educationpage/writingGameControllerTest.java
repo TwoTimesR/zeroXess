38807a25c6bc9b3ab95fe7d703dcf377f2835ec5
2R <63011073+RichalRambaran@users.noreply.github.com>
2020-06-16 18:31:54 +0200

    slightly changed writingGameControllerTest as well

--- Test/com/zeroxess/educationpage/writingGameControllerTest.java
+++ Test/com/zeroxess/educationpage/writingGameControllerTest.java
@@ -4,70 +4,91 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class writingGameControllerTest {
+    WritingGame game = new WritingGame("Test");
+
+    private void writingGameSetup() {
+        SingularQuestion question0 = new SingularQuestion("1.What is the name of this fruit?", "apple");
+        SingularQuestion question1 = new SingularQuestion("2.What is the name of this insect?", "bee");
+        SingularQuestion question2 = new SingularQuestion("3.Which country does this flag belong to?", "japan");
+        SingularQuestion question3 = new SingularQuestion("4.What is the name of this food?", "hamburger");
+        SingularQuestion question4 = new SingularQuestion("5.What is the name of this animal?", "lion");
+        SingularQuestion question5 = new SingularQuestion("6.What is the name of this insect?", "spider");
+        SingularQuestion question6 = new SingularQuestion("7.What do you call this vehicle?", "car");
+        SingularQuestion question7 = new SingularQuestion("8.What are they doing?", "dancing");
+        SingularQuestion question8 = new SingularQuestion("9.What programming language is this?", "java");
+        game.addSingularQuestion(question0);
+        game.addSingularQuestion(question1);
+        game.addSingularQuestion(question2);
+        game.addSingularQuestion(question3);
+        game.addSingularQuestion(question4);
+        game.addSingularQuestion(question5);
+        game.addSingularQuestion(question6);
+        game.addSingularQuestion(question7);
+        game.addSingularQuestion(question8);
+    }
 
     @Test
     public void questionTest() {
+        writingGameSetup();
         SingularQuestion question = new SingularQuestion("1.What is the name of this fruit?","apple");
-        WritingGame game = new WritingGame("test");
 
         System.out.println(game.getSingularQuestions().get(0).getQuestion());
-        assertEquals(game.getSingularQuestions().get(0).getQuestion(),question.getQuestion());
+        assertEquals(game.getSingularQuestions().get(0).getQuestion(), question.getQuestion());
     }
 
-
     @Test
-    public void answerTest(){
-        WritingGame game = new WritingGame("test1");
+    public void answerTest() {
+        writingGameSetup();
         String ant = "apple";
-        assertEquals(game.getSingularQuestions().get(0).getSingularAnswer().getCorrectAnswer(),ant);
+        assertEquals(game.getSingularQuestions().get(0).getSingularAnswer().getCorrectAnswer(), ant);
     }
     @Test
-    public void answerTest2(){
-        WritingGame game = new WritingGame("test2");
+    public void answerTest2() {
+        writingGameSetup();
         String ant2 = "bee";
-        assertEquals(game.getSingularQuestions().get(1).getSingularAnswer().getCorrectAnswer(),ant2);
+        assertEquals(game.getSingularQuestions().get(1).getSingularAnswer().getCorrectAnswer(), ant2);
     }
     @Test
-    public void answerTest3(){
-        WritingGame game = new WritingGame("test3");
+    public void answerTest3() {
+        writingGameSetup();
         String ant3 = "japan";
-        assertEquals(game.getSingularQuestions().get(2).getSingularAnswer().getCorrectAnswer(),ant3);
+        assertEquals(game.getSingularQuestions().get(2).getSingularAnswer().getCorrectAnswer(), ant3);
     }
     @Test
-    public void answerTest4(){
-        WritingGame game = new WritingGame("test4");
+    public void answerTest4() {
+        writingGameSetup();
         String ant4 = "hamburger";
-        assertEquals(game.getSingularQuestions().get(3).getSingularAnswer().getCorrectAnswer(),ant4);
+        assertEquals(game.getSingularQuestions().get(3).getSingularAnswer().getCorrectAnswer(), ant4);
     }
     @Test
-    public void answerTest5(){
-        WritingGame game = new WritingGame("test5");
+    public void answerTest5() {
+        writingGameSetup();
         String ant5 = "lion";
-        assertEquals(game.getSingularQuestions().get(4).getSingularAnswer().getCorrectAnswer(),ant5);
+        assertEquals(game.getSingularQuestions().get(4).getSingularAnswer().getCorrectAnswer(), ant5);
     }
     @Test
-    public void answerTest6(){
-        WritingGame game = new WritingGame("test6");
-    String ant6 = "spider" ;
-    assertEquals(game.getSingularQuestions().get(5).getSingularAnswer().getCorrectAnswer(),ant6);
+    public void answerTest6() {
+        writingGameSetup();
+        String ant6 = "spider" ;
+        assertEquals(game.getSingularQuestions().get(5).getSingularAnswer().getCorrectAnswer(), ant6);
     }
     @Test
-    public void answerTest7(){
-        WritingGame game = new WritingGame("test7");
+    public void answerTest7() {
+        writingGameSetup();
         String ant7 = "car";
-        assertEquals(game.getSingularQuestions().get(6).getSingularAnswer().getCorrectAnswer(),ant7);
+        assertEquals(game.getSingularQuestions().get(6).getSingularAnswer().getCorrectAnswer(), ant7);
     }
     @Test
-    public void answerTest8(){
-        WritingGame game = new WritingGame("test8");
+    public void answerTest8() {
+        writingGameSetup();
         String ant8 ="dancing";
-        assertEquals(game.getSingularQuestions().get(7).getSingularAnswer().getCorrectAnswer(),ant8);
+        assertEquals(game.getSingularQuestions().get(7).getSingularAnswer().getCorrectAnswer(), ant8);
     }
     @Test
-    public void answertest9(){
-        WritingGame game = new WritingGame("test9");
+    public void answerTest9() {
+        writingGameSetup();
         String ant9 = "java";
-        assertEquals(game.getSingularQuestions().get(8).getSingularAnswer().getCorrectAnswer(),ant9);
+        assertEquals(game.getSingularQuestions().get(8).getSingularAnswer().getCorrectAnswer(), ant9);
     }
 
 }
